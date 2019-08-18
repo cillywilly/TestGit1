1e43ed92509caa4ea2bfacdda20047e352ee9c8c
mokol <mokolot@mail.ru>
2019-08-18 21:32:19 +0300

    Test commit3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         Date currentTime = new Date();
-
-
         System.out.println("Time from midnight " +currentTime);
-
-	// write your code here
     }
 }
