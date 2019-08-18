f918092270327693a4e631ecd01a70c434dff61f
mokol <mokolot@mail.ru>
2019-08-18 21:27:11 +0300

    Test commit3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         Date currentTime = new Date();
-        int hours = currentTime.getHours();
-        int mins = currentTime.getMinutes();
-        int secs = currentTime.getSeconds();
 
-        System.out.println("Time from midnight " + hours + ":" + mins + ":" + secs);
+
+        System.out.println("Time from midnight " +currentTime);
 
 	// write your code here
     }
