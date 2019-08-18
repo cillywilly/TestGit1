3572be2e8a5d74ce690c1e40f70317082a797e0a
mokol <mokolot@mail.ru>
2019-08-18 21:24:13 +0300

    Test commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,17 @@
 package com.company;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Date currentTime = new Date();
+        int hours = currentTime.getHours();
+        int mins = currentTime.getMinutes();
+        int secs = currentTime.getSeconds();
+
+        System.out.println("Time from midnight " + hours + ":" + mins + ":" + secs);
 
 	// write your code here
     }
